864eb87106cf2eed1bdc7b3340e79369bf0ca10d
lucascdri <93845714+lucascdri@users.noreply.github.com>
2022-06-21 16:44:33 +0100

    Setting up socket on server

---
+++ server.js
@@ -0,0 +1,41 @@
+const net = require('net');
+
+let sockets = [];
+
+const server = net.createServer(socket => {
+    sockets.push(socket);
+    console.log("A Client has connected");
+
+    socket.on('data', data => {
+        sendOut(data, socket);
+    });
+
+    socket.on('error', err => {
+        console.log("A Client has disconnected")
+    });
+
+    socket.on('close', () => {
+        console.log("A Client has left the chat");
+    } )
+});
+
+function sendOut (message, socketSent) {
+
+    if (message.toString() === "quit") {
+
+        const delIndex = sockets.indexOf(socketSent);
+        sockets.splice(delIndex, 1);
+
+    } else {
+
+        sockets.forEach(socket => {
+            
+            if (socket !== socketSent) {
+                socket.write(message);
+            }
+
+        });
+    }
+}
+
+server.listen(3000);
